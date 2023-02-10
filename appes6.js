f082c1dba085b883388c8b14e867367d7594a772
F Bliss <60014136+voffee@users.noreply.github.com>
2023-02-10 22:31:56 +0800

    Local storage complete

--- appes6.js
+++ appes6.js
@@ -56,6 +56,55 @@ class UI {
     }
 }
 
+// Local Storage Class
+class Store {
+    static getBooks() {
+        let books;
+        if(localStorage.getItem('books') === null) {
+            books = [];
+        } 
+        else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+    
+    static displayBooks() {
+        const books = Store.getBooks();
+        
+        books.forEach(function(book){
+            const ui = new UI;
+
+            // Add book to UI
+            ui.addBookToList(book);
+        });
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks();
+
+        books.push(book);
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn) {
+        const books = Store.getBooks();
+
+        books.forEach(function(book, index){
+            if(book.isbn === isbn) {
+                books.splice(index, 1);
+            }
+        });
+        
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
+
 // Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', 
 function(e) {
@@ -78,6 +127,9 @@ function(e) {
         // Add book to list
         ui.addBookToList(book);
 
+        // Add to LS
+        Store.addBook(book);
+
         // Show sucess
         ui.showAlert('Book Added!', 'success');
         // Clear fields
@@ -94,6 +146,9 @@ document.getElementById('book-list').addEventListener('click', function(e){
 
     // Delete book
     ui.deleteBook(e.target);
+
+    // Remove from LS
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
     // Show message
     ui.showAlert('Book Removed!', 'success');
